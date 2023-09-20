af14b6d81e8b716d302017b8734aee6fda7458d4
jonjaffary <jonjaffary34@gmail.com>
2023-09-20 05:04:01 +0500

    merge

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-nums = [3, 2, 5, 14, 5, 5, 19, 18, 11, 10, 1, 4, 5, 5, 5, 5, 12, 5, 17, 5];
-let sortedArray = nums.sort((a, b) => a - b);
+let sortedArray = nums.sort();
 for (let i = 0; i < sortedArray.length; i++) {
     if (sortedArray[i + 1] > sortedArray[i]) {
 
     } else {
-        console.log(sortedArray[i])
+        return sortedArray[i]
     }
 }
